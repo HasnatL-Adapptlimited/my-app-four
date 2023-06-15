94054088700149358d10e1d4d6972950625b9f11
Hasnat Lamina <136315346+HasnatL-Adapptlimited@users.noreply.github.com>
2023-06-15 01:30:06 +0100

    Detail component recreated when id route clicked

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
 import { ProductsService } from '../products.service';
 import { Product } from '../product';
 import { AuthService } from '../../auth/auth.service';
@@ -9,7 +10,7 @@ import { AuthService } from '../../auth/auth.service';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnChanges {
+export class ProductDetailComponent implements OnInit, OnChanges {
 
   @Input() product: Product | undefined;
   @Output() bought = new EventEmitter();
@@ -17,7 +18,18 @@ export class ProductDetailComponent implements OnChanges {
   @Input() id = -1;
   product$: Observable<Product> | undefined;
 
-  constructor(private productService: ProductsService, public authService: AuthService) { }
+  constructor(
+    private productService: ProductsService,
+    public authService: AuthService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.product$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        return this.productService.getProduct(Number(params.get('id')));
+      })
+    );
+  }
 
   ngOnChanges(): void {
    this.product$ = this.productService.getProduct(this.id);
