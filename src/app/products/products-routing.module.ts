b56d070861eb69c1f1645cef7efbda4edcf10887
Hasnat Lamina <136315346+HasnatL-Adapptlimited@users.noreply.github.com>
2023-06-15 01:12:09 +0100

    **space added

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -4,7 +4,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: '', redirectTo: '/products', pathMatch:'full'}
+  { path: '', redirectTo: '/products', pathMatch:'full' }
 ];
 
 @NgModule({
