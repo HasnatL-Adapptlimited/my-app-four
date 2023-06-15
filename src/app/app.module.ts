7fbff3d3f590237be179cb26c58af97d138bc5ae
Hasnat Lamina <136315346+HasnatL-Adapptlimited@users.noreply.github.com>
2023-06-15 01:08:12 +0100

    Page not exist error handling catching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductsModule } from './products/products.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
